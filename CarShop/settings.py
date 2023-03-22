5afbc231d29f7f05d2efa169f43a1a79aab02a93
Tri Ta Minh <minhtrita410@gmail.com>
2023-03-23 03:55:14 +0700

    change to db.sqlite3

--- CarShop/settings.py
+++ CarShop/settings.py
@@ -103,12 +103,12 @@ MAX_CONN_AGE = 600
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db_carshop',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'USER': 'root',
+        # 'PASSWORD': '',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '3306',
     }
 }
 
